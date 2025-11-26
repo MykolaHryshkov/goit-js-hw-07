5371bd1e0acde2b9e4d583a32118c2d063adea15
unknown <grishkovnikola@gmail.com>
2025-11-26 21:23:18 +0200

    add alt

--- js/task-5.js
+++ js/task-5.js
@@ -4,18 +4,12 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+const body = document.body;
 
-const btn = document.querySelector('.change-color');
-const colorSpan = document.querySelector('.color');
-
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   const color = getRandomHexColor();
-
-  document.body.style.backgroundColor = color;
-  colorSpan.textContent = color;
+  body.style.backgroundColor = color;
+  span.textContent = color;
 });
